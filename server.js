40b357500ec2da71c844bf71b55fd2550cb134b8
vntruong1999 <56061432+vntruong1999@users.noreply.github.com>
2020-11-06 18:38:53 -0500

    Updated

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ io.on("connection", async (socket) => {
         receiver: messageReceiver,
         content: messageContent,
         chatRoomId: chatRoomId,
-        messageId: messagId,
+        _id: messagId,
       };
 
       // Emit this event so that the client app will get update when new message is added
