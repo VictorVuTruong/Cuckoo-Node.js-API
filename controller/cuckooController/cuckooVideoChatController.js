db53cce27457d4ad0bd14722100a332c55de7ee2
vntruong1999 <56061432+vntruong1999@users.noreply.github.com>
2021-03-21 17:49:26 -0400

    Updated video chat controllers

--- controller/cuckooController/cuckooVideoChatController.js
+++ controller/cuckooController/cuckooVideoChatController.js
@@ -1,7 +1,7 @@
 const { request, response } = require("express");
 
 // Import twillio SDK
-const twillio = require('twilio');
+const twillio = require("twilio");
 
 // Import video grant class
 const { VideoGrant } = require("twilio/lib/jwt/AccessToken");
@@ -17,74 +17,109 @@ const catchAsync = require(`${__dirname}/../../utils/catchAsync`);
 
 // The function to grant access for user with specified user id to get into the video chat room
 exports.grantVideoChatAccess = catchAsync(async (request, response, next) => {
-    // Get user id of the user requesting for token
-    const userId = request.query.userId
+  // Get user id of the user requesting for token
+  const userId = request.query.userId;
 
-    // Get room id of room that user want to join
-    const roomId = request.query.roomId
+  // Get room id of room that user want to join
+  const roomId = request.query.roomId;
 
-    // Get account SID
-    const accountSID = process.env.TWILIO_ACCOUNT_SID
+  // Get account SID
+  const accountSID = process.env.TWILIO_ACCOUNT_SID;
 
-    // Get API key SID
-    const apiKeySID = process.env.TWILIO_API_KEY_SID
+  // Get API key SID
+  const apiKeySID = process.env.TWILIO_API_KEY_SID;
 
-    // Get API key secret
-    const apiKeySecret = process.env.TWILIO_API_KEY_SECRET
+  // Get API key secret
+  const apiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
-    // Create access token
-    const accessToken = new AccessToken(
-        accountSID,
-        apiKeySID,
-        apiKeySecret
-    )
+  // Create access token
+  const accessToken = new AccessToken(accountSID, apiKeySID, apiKeySecret);
 
-    // Set identity for the token (user id of the user requesting for token)
-    accessToken.identity = userId
+  // Set identity for the token (user id of the user requesting for token)
+  accessToken.identity = userId;
 
-    // Grant access to the token
-    const grant = new VideoGrant()
-    //grant.room = roomId
-    accessToken.addGrant(grant)
+  // Grant access to the token
+  const grant = new VideoGrant();
+  //grant.room = roomId
+  accessToken.addGrant(grant);
 
-    // Serialize token as a JWT
-    const jwt = accessToken.toJwt()
-    
-    // Return response to the client requesting for access token
-    response.status(200).json({
-        accessToken: jwt
-    })
-})
+  // Serialize token as a JWT
+  const jwt = accessToken.toJwt();
+
+  // Return response to the client requesting for access token
+  response.status(200).json({
+    accessToken: jwt,
+  });
+});
 
 // The function to create a room
 exports.createRoom = catchAsync(async (request, response, next) => {
-    // Get name of the chat room to create
-    const chatRoomName = request.query.chatRoomName
-    
-    // Get account SID
-    const accountSID = process.env.TWILIO_ACCOUNT_SID
+  // Get name of the chat room to create
+  const chatRoomName = request.query.chatRoomName;
 
-    // Get account auth token
-    const accountAuthToken = process.env.TWILIO_AUTH_TOKEN
+  // Get account SID
+  const accountSID = process.env.TWILIO_ACCOUNT_SID;
 
-    // Create client object
-    const client = require('twilio')(accountSID, accountAuthToken)
+  // Get account auth token
+  const accountAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
-    // Create room
-    client.video.rooms
-        .create({
-            type: 'go',
-            uniqueName: chatRoomName
-        })
-        .then(room => {
-            console.log("Done")
-            
-            response.status(201).json({
-                status: "Success. Room created",
-                name: chatRoomName,
-                roomSID: room.sid
-            })
-        }).catch(error => {
-            console.log(error)
-        })
-})
+  // Create client object
+  const client = require("twilio")(accountSID, accountAuthToken);
+
+  // Create room
+  client.video.rooms
+    .create({
+      type: "go",
+      uniqueName: chatRoomName,
+    })
+    .then((room) => {
+      // Return response and let the client know that room has been created
+      response.status(201).json({
+        status: "Success. Room created",
+        name: chatRoomName,
+        roomSID: room.sid,
+      });
+    })
+    .catch((error) => {
+      console.log(error.code === 53113);
+      // Return response and let the client know that room is already existed
+      response.status(400).json({
+        status: "Room existed",
+        name: chatRoomName,
+      });
+    });
+});
+
+// The function to complete a room (delete a room)
+exports.completeRoom = catchAsync(async (request, response, next) => {
+  // Get room name to complete
+  const chatRoomName = request.query.chatRoomName;
+
+  // Get account SID
+  const accountSID = process.env.TWILIO_ACCOUNT_SID;
+
+  // Get account auth token
+  const accountAuthToken = process.env.TWILIO_AUTH_TOKEN;
+
+  // Create client object
+  const client = require("twilio")(accountSID, accountAuthToken);
+
+  // Call the function to end a room
+  client.video
+    .rooms(chatRoomName)
+    .update({ status: "completed" })
+    .then((room) => {
+      // Return response to the client so that client know that room has been ended
+      response.status(200).json({
+        status: "Success. Room ended",
+        name: chatRoomName,
+      });
+    })
+    .catch((error) => {
+      if (error.status === 404) {
+        response.status(404).json({
+          status: "Room not found",
+        });
+      }
+    });
+});
--- routes/cuckooMainRoute/cuckooVideoChatRoutes.js
+++ routes/cuckooMainRoute/cuckooVideoChatRoutes.js
@@ -1,5 +1,5 @@
 // Import express for the route
-const express = require("express")
+const express = require("express");
 
 // Import the authenticationController
 const authenticationController = require(`${__dirname}/../../controller/authenticationController`);
@@ -8,20 +8,21 @@ const authenticationController = require(`${__dirname}/../../controller/authenti
 const cuckooVideoChatController = require(`${__dirname}/../../controller/cuckooController/cuckooVideoChatController`);
 
 // Create router for the video chat
-const router = express.Router()
+const router = express.Router();
 
 // Use the protect middleware to protect any routes beyond this point
-router.use(authenticationController.protect)
+router.use(authenticationController.protect);
 
 // The route for getting access token to get into chat room
 router
-    .route("/getAccessToken")
-    .get(cuckooVideoChatController.grantVideoChatAccess)
+  .route("/getAccessToken")
+  .get(cuckooVideoChatController.grantVideoChatAccess);
 
 // The route for creating new chat room
-router
-    .route("/createRoom")
-    .post(cuckooVideoChatController.createRoom)
+router.route("/createRoom").post(cuckooVideoChatController.createRoom);
+
+// The route for ending a chat room
+router.route("/endRoom").delete(cuckooVideoChatController.completeRoom);
 
 // Export the module
 module.exports = router;
